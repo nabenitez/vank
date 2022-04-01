1b61fb52376ee70fead0c9f1e8ed46be63a7dbb2
nabenitez <nabenitez@uc.cl>
2022-04-01 13:21:47 -0300

    feat(api): add validations to create client router

--- apps/api/src/app/presentation/routes/client-router.ts
+++ apps/api/src/app/presentation/routes/client-router.ts
@@ -1,33 +1,30 @@
-import { IClient } from '@vank/shared-types';
 import { Request, Response, Router } from 'express';
 import { CreateClientUseCase } from '../../domain/interfaces/use-cases/create-client';
+import {
+  getClientValidations,
+  getFilteredClient,
+} from '../../domain/use-cases/client/client-validations';
+import { validate } from '@vank/request-validator';
 
 export default function ClientsRouter(
   createClientUseCase: CreateClientUseCase
 ) {
   const router = Router();
 
-  router.post('/', async (req: Request, res: Response) => {
-    try {
-      const filteredClient = getFilteredClient(req.body);
-      await createClientUseCase.execute(filteredClient);
-      res.statusCode = 201;
-      res.json({ message: 'client created' });
-    } catch (err) {
-      res.status(500).send({ message: 'error creating client' });
+  router.post(
+    '/',
+    validate(getClientValidations()),
+    async (req: Request, res: Response) => {
+      try {
+        const filteredClient = getFilteredClient(req.body);
+        await createClientUseCase.execute(filteredClient);
+        res.statusCode = 201;
+        res.json({ message: 'client created' });
+      } catch (err) {
+        res.status(500).send({ message: 'error creating client' });
+      }
     }
-  });
+  );
 
   return router;
-}
-
-function getFilteredClient(requestBody: IClient): IClient {
-  return {
-    companyName: requestBody.companyName,
-    internalCode: requestBody.internalCode,
-    tributaryId: requestBody.tributaryId,
-    currency: requestBody.currency,
-    monthlyApiCallsFee: requestBody.monthlyApiCallsFee,
-    allowedBanks: requestBody.allowedBanks,
-  };
 }
