1ec72bbf8c943f070252f36a2cc4d7aa9a7253e1
nabenitez <nabenitez@uc.cl>
2022-03-31 23:22:04 -0300

    chore: add clean architecture folders structure

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,11 @@
 
 import * as express from 'express';
 
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+(async () => {
+  const app = express();
+  const port = process.env.PORT || 3333;
+  const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+  });
+  server.on('error', console.error);
+})();
