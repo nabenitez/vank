a2da7fcb2798a778ed93f08721c27e95436a9dc4
nabenitez <nabenitez@uc.cl>
2022-04-01 09:48:43 -0300

    feat(api): add db datasource and  client middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,21 @@
  */
 
 import server from './app/infrastructure/server';
+import ClientRouter from './app/presentation/routes/client-router';
+import { ClientRepositoryImpl } from './app/domain/repositories/client-repository';
+import { CreateClient } from './app/domain/use-cases/client/create-client';
+import { getMongoDBDS } from './app/infrastructure/mongodb-connection';
 
 (async () => {
+  const clientDataSource = await getMongoDBDS();
+  const clientMiddleware = ClientRouter(
+    new CreateClient(new ClientRepositoryImpl(clientDataSource))
+  );
   const port = process.env.PORT || 3333;
   server.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/api`);
   });
   server.on('error', console.error);
+
+  server.use('/client', clientMiddleware);
 })();
