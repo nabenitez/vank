87bd4f0f9d827ef8bd7b3e62011a0ffa4af6935b
nabenitez <nabenitez@uc.cl>
2022-04-01 15:11:06 -0300

    fix(api): add update client to middleware

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,14 @@ import server from './app/infrastructure/server';
 import ClientRouter from './app/presentation/routes/client-router';
 import { ClientRepositoryImpl } from './app/domain/repositories/client-repository';
 import { CreateClient } from './app/domain/use-cases/client/create-client';
+import { UpdateClient } from './app/domain/use-cases/client/update-client';
 import { getMongoDBDS } from './app/infrastructure/mongodb-connection';
 
 (async () => {
   const clientDataSource = await getMongoDBDS();
   const clientMiddleware = ClientRouter(
-    new CreateClient(new ClientRepositoryImpl(clientDataSource))
+    new CreateClient(new ClientRepositoryImpl(clientDataSource)),
+    new UpdateClient(new ClientRepositoryImpl(clientDataSource))
   );
   const port = process.env.PORT || 3333;
   server.listen(port, () => {
