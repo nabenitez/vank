779d486072624a8243ab2916d9e831a3665ebda0
nabenitez <nabenitez@uc.cl>
2022-04-03 19:00:34 -0400

    fix(api): update mongo initialization

--- apps/api/src/app/infrastructure/mongodb-connection.ts
+++ apps/api/src/app/infrastructure/mongodb-connection.ts
@@ -4,12 +4,9 @@ import { MongoDBClientDataSource } from '../data/data-sources/mongodb-client-dat
 const { MONGO_DB_URL } = process.env;
 
 export async function getMongoDBDS() {
-  const client: MongoClient = new MongoClient(
-    MONGO_DB_URL || 'mongodb://localhost:27020',
-    {
-      serverApi: ServerApiVersion.v1,
-    }
-  );
+  const client: MongoClient = new MongoClient(MONGO_DB_URL, {
+    serverApi: ServerApiVersion.v1,
+  });
   await client.connect();
 
   const clientsDB = client.db('clients_db');
