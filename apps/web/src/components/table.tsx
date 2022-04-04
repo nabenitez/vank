a98fb656f86d7eb2f04afb35133ed5efaf678a8a
nabenitez <nabenitez@uc.cl>
2022-04-04 05:49:21 -0400

    feat(web): add table wrapper

---
+++ apps/web/src/components/table.tsx
@@ -0,0 +1,40 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import React from 'react';
+
+const TableWrapper = ({ headers, rows }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            {headers.map((header, index) => (
+              <TableCell key={index}>{header}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow
+              key={index}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              {Object.entries(row).map((item, index) => (
+                <TableCell key={index}>{item[1]}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TableWrapper;
